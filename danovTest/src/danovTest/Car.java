0197163ad80bc38b08d483dfe63052c034a3ea70
aposlan <98400150+aposlan@users.noreply.github.com>
2023-02-13 23:21:20 +0200

    Add files via upload

---
+++ danovTest/src/danovTest/Car.java
@@ -0,0 +1,9 @@
+package danovTest;
+
+public class Car {
+
+	Car(){
+		
+	}
+	
+}
---
+++ danovTest/src/danovTest/Test.java
@@ -0,0 +1,13 @@
+package danovTest;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> array = new ArrayList<>();
+		
+	}
+
+}
